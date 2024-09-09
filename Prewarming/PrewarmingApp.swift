170a6df05bda4ab06fb164cc5a6023c335c09730
Jonatan Nielavitzky <ioni@shape.dk>
2024-09-09 15:03:09 +0200

    Initial Commit

---
+++ Prewarming/PrewarmingApp.swift
@@ -0,0 +1,17 @@
+//
+//  PrewarmingApp.swift
+//  Prewarming
+//
+//  Created by Jonatan Nielavitzky on 09/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct PrewarmingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
